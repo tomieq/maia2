4d3a13975364838c1c9f80c2cb0eedaae8e01a42
tomieq <tomieq@wp.pl>
2026-03-27 17:57:49 +0100

    Use local model

--- maia2/model.py
+++ maia2/model.py
@@ -10,30 +10,14 @@ import pdb
 
 def from_pretrained(type, device, save_root = "./maia2_models"):
     
-    if os.path.exists(save_root) == False:
-        os.makedirs(save_root)
-    
-    if type == "blitz":
-        url = "https://drive.google.com/uc?id=1X-Z4J3PX3MQFJoa8gRt3aL8CIH0PWoyt"
-        output_path = os.path.join(save_root, "blitz_model.pt")
-    
-    elif type == "rapid":
-        url = "https://drive.google.com/uc?id=1gbC1-c7c0EQOPPAVpGWubezeEW8grVwc"
-        output_path = os.path.join(save_root, "rapid_model.pt")
-    
-    else:
-        raise ValueError("Invalid model type. Choose between 'blitz' and 'rapid'.")
+    output_path = os.path.join(save_root, "rapid_model.pt")
 
-    if os.path.exists(output_path):
-        print(f"Model for {type} games already downloaded.")
-    else:
-        print(f"Downloading model for {type} games.")
-        gdown.download(url, output_path, quiet=False)
+    if not os.path.exists(output_path):
+        raise ValueError("Model at " + output_path + " does not exist")
 
-    cfg_url = "https://drive.google.com/uc?id=1GQTskYMVMubNwZH2Bi6AmevI15CS6gk0"
     cfg_path = os.path.join(save_root, "config.yaml")
     if not os.path.exists(cfg_path):
-        gdown.download(cfg_url, cfg_path, quiet=False)
+        raise ValueError("Config does not exist")
 
     cfg = parse_args(cfg_path)
 
